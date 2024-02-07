d629ad52a306bee4e798e303be29bf740ba122e3
mrbkd11 <medbkd11@gmail.com>
2024-02-07 15:44:16 +0100

    adding mobile app

---
+++ mobileapp/android/app/src/main/kotlin/com/hypeteq/watermaniac/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.hypeteq.watermaniac
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ mobileapp/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:fitnessapp/routes.dart';
+import 'package:fitnessapp/utils/app_colors.dart';
+import 'package:fitnessapp/view/login/login_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fitness',
+      debugShowCheckedModeBanner: false,
+      routes: routes,
+      theme: ThemeData(
+          primaryColor: AppColors.primaryColor1,
+          useMaterial3: true,
+          fontFamily: "Poppins"),
+      home: const LoginScreen(),
+    );
+  }
+}
